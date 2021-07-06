61e28a6c6fed622801a30b664336dd2806af1a53
unknown <taranjeet@valuecreatives.com>
2021-07-06 23:09:00 +0530

    code update

--- frontend/my-app/src/pages/Auth.js
+++ frontend/my-app/src/pages/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { DialogComponent } from '@syncfusion/ej2-react-popups';
 import './Auth.css';
 import AuthContext from '../context/auth-context';
 
@@ -14,6 +14,40 @@ class AuthPage extends Component {
     super(props);
     this.emailEl = React.createRef();
     this.passwordEl = React.createRef();
+    this.buttons = [{
+      buttonModel: {
+          content: 'DISMISS',
+          cssClass: 'e-primary',
+          isPrimary: true,
+      },
+      'click': () => {
+          this.dialogInstance.hide();
+      }
+  }];
+    this.validation = (args) => {
+      if (this.emailEl.value === "" && this.passwordEl.value === "") {
+          args.cancel = true;
+          alert("Enter the E-mail and password");
+      }
+      else if (this.emailEl.value === "") {
+          args.cancel = true;
+          alert("Enter the email");
+      }
+      else if (this.passwordEl.value === "") {
+          args.cancel = true;
+          alert("Enter the password");
+      }
+      else if (this.passwordEl.value.length < 4) {
+          args.cancel = true;
+          alert("Password must be minimum 4 characters");
+      }
+      else {
+          args.cancel = false;
+          this.userName.value = "";
+          this.password.value = "";
+      }
+  };
+
   }
 
   switchModeHandler = () => {
@@ -22,6 +56,9 @@ class AuthPage extends Component {
     });
   };
 
+  onSubmit() {
+    this.dialogInstance.show();
+}
   submitHandler = event => {
     event.preventDefault();
     const email = this.emailEl.current.value;
@@ -47,14 +84,14 @@ class AuthPage extends Component {
         // }
       };
     }
-console.log("xhvjhgc",requestBody)
+    console.log("xhvjhgc", requestBody)
     fetch('http://localhost:8000/graphql', {
       method: 'POST',
       body: JSON.stringify(requestBody),
       headers: new Headers({
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-    }),
+      }),
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -64,6 +101,14 @@ console.log("xhvjhgc",requestBody)
       })
       .then(resData => {
         console.log(resData);
+        if (resData.data.createUser) {
+
+          
+          console.log("signup Successfull");
+        }
+        else {
+
+        }
         if (resData.data.login.token) {
           this.context.login(
             resData.data.login.token,
@@ -76,6 +121,8 @@ console.log("xhvjhgc",requestBody)
         console.log(err);
       });
   };
+
+ 
 
   render() {
     return (
@@ -90,10 +137,11 @@ console.log("xhvjhgc",requestBody)
         </div>
         <div className="form-actions">
           <button type="submit">Submit</button>
-          <button type="button" onClick={this.switchModeHandler}>
+          <button type="button" onClick={this.onSubmit = this.onSubmit.bind(this)}>
             Switch to {this.state.isLogin ? 'Signup' : 'Login'}
           </button>
         </div>
+        <DialogComponent id='dialog' header='Success' buttons={this.buttons} beforeOpen={this.validation} content='Congratulations! Login Success' width='250px' isModal={true} ref={dialog => this.dialogInstance = dialog} visible={false} target='#dialog-target'/>
       </form>
     );
   }
